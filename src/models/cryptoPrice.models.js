020d99fd53707868a8e86e283fba49c162694949
vandana41 <vandana4171@gmail.com>
2025-01-11 01:19:25 +0530

    a

--- src/models/cryptoPrice.models.js
+++ src/models/cryptoPrice.models.js
@@ -2,12 +2,29 @@
 const mongoose = require("mongoose");
 
 const cryptoPriceSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  symbol: { type: String, required: true },
-  priceUSD: { type: Number, required: true },
-  marketCapUSD: { type: Number, required: true },
-  change24h: { type: Number, required: true },
-  timestamp: { type: Date, default: Date.now }
+  name: { 
+    type: String, 
+    required: true 
+  },
+  symbol: { 
+    type: String, 
+    required: true 
+  },
+  priceUSD: { 
+    type: Number, 
+    required: true 
+  },
+  marketCapUSD: { 
+    type: Number, 
+    required: true 
+  },
+  change24h: { 
+    type: Number, 
+    required: true 
+  },
+  timestamp: { 
+    type: Date, 
+    default: Date.now }
 });
 
 module.exports = mongoose.model("CryptoPrice", cryptoPriceSchema);
